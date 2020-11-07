de52798d37f5b96e7231dea64ba74c46d4046552
Rafael Alves <rafael.alves.batista@gmail.com>
2020-11-07 03:23:30 -0300

    Incluindo a pasta include e modulando o código

--- Cond_Ruido/Ortogonal/Ortogonal.cpp
+++ Cond_Ruido/Ortogonal/Ortogonal.cpp
@@ -7,44 +7,10 @@
 #include <ctime>
 #include <fstream>
 #include <chrono>
+#include "../../include/Ham_O.h"
 
 using namespace Eigen;
 using namespace std::literals;
-
-void Criando_H (MatrixXcd *H_pointer, int ress, double V){
-	
-	auto seed = std::chrono::system_clock::now().time_since_epoch().count();	
-  	std::normal_distribution<double> distribution(0.0,1.0);
-	std::default_random_engine generator(seed);
-	
-	MatrixXcd A(ress,ress); 
-	MatrixXcd H1(ress,ress);
-	MatrixXcd Simetrica(ress,ress);
-
-	A.setZero();
-	H1.setZero();
-	Simetrica.setZero();
-	
-	for (int i = 1; i < ress + 1; i++){
-		for (int j = 1; j < ress + 1; j++){
-			double aux = distribution(generator);
-			A(i-1,j-1) = aux;
-		}
-	}
-
-	for (int i = 1; i < ress + 1; i++){
-		H1(i-1,i-1) = A(i-1,i-1)*sqrt(V/(2.0));
-		for (int j = i + 1; j < ress + 1; j++){
-			H1(i-1,j-1) = A(i-1,j-1)*sqrt(V/(1.0));
-		}
-	}
-
-	Simetrica << H1 + H1.adjoint();
-
-	MatrixXcd H = Simetrica;
-	*H_pointer = H;
-	
-}
 
 void Criando_W (MatrixXcd *W_pointer, int ress, int N1, int N2, double lambda, double y){
 
@@ -150,6 +116,7 @@ int main(){
 	MatrixXcd identityS(n,n);
 	MatrixXcd G(num_realization,1);
 	MatrixXcd R(num_realization,1);
+
 	// Pauli Matrices //
 
 	MatrixXcd matrizpauli1(2,2);
@@ -222,8 +189,8 @@ int main(){
 	std::ofstream output_R("R_O.txt");
 	for (int i = 0; i < num_realization; i++){
 		for (int j = 0; j < 1; j++){
-			output_G << G(i,j).real() << " " << std::endl;
-			output_R << R(i,j).real() << " " << std::endl;
+			output_G << G(i,j).real() << "\t" << std::endl;
+			output_R << R(i,j).real() << "\t" << std::endl;
 		}
 	}
 	return 0;
---
+++ include/Ham_O.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#ifndef HAM_O_H
+#define HAM_O_H
+
+using namespace std;
+using namespace Eigen;
+using namespace std::literals;
+
+void Criando_H (MatrixXcd *H_pointer, int ress, double V);
+
+#endif
