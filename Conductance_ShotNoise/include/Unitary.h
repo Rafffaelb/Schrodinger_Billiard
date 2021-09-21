c602a4b356264b53daf844d8d1fc6fa61b419504
Rafffaelb <rafael.alves.batista@gmail.com>
2021-09-21 16:11:43 -0300

    Creating Subclass Unitary

---
+++ Conductance_ShotNoise/include/Unitary.h
@@ -0,0 +1,21 @@
+#ifndef UNITARY_H
+#define UNITARY_H
+
+#include "WignerDyson.h"
+
+class Unitary: public WignerDyson{
+
+	public:
+		Unitary(double Gamma, int ress, double lambda, int num_steps, int spin_deg);
+
+		~Unitary();
+
+		void Create_W(MatrixXcd* W_pointer, int _ress, int N1, int N2, double _lambda, double _y);
+		
+		void Create_ProjectionMatrices(MatrixXcd *C1_pointer, MatrixXcd* C2_pointer, int N1, int N2);
+
+		void Create_H(MatrixXcd* H_pointer, int ress, double V);
+		void Save_txt_files(MatrixXcd G, MatrixXcd P, int num_steps);
+};
+
+#endif
